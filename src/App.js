cbb7126e2db5e924fa02e66bd3cd39f3ebc2bc85
Biljana Jevtic <biljanajevtic@Biljanas-MacBook-Pro.local>
2021-02-02 22:14:18 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from "react";
+import $ from 'jquery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quote/>
     </div>
   );
 }
 
 export default App;
+
+class Quote extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: '',
+      tweetLink: ''
+    }
+    
+    this.getQuote = this.getQuote.bind(this);
+    this.getQuote();
+  }
+
+  getQuote(){
+  
+    $.ajax({
+      headers: {
+        Accept:'application/json'
+      },
+      url: 'https://goquotes-api.herokuapp.com/api/v1/random?count=1',
+      success: (jsonQuotes) => {
+        if(jsonQuotes.status === 200 && jsonQuotes.count === 1){
+  
+          let newQuote = {
+            quote: jsonQuotes.quotes[0].text,
+            author: jsonQuotes.quotes[0].author
+          };
+          
+          this.setState(() => ({
+            quote: newQuote.quote,
+            author: newQuote.author,
+            tweetLink: 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + 
+            encodeURIComponent('"' + newQuote.quote + '" ' + newQuote.author)          
+          }));
+          
+        }
+      }
+    });
+    
+  };
+
+  render(){
+    return(
+      <div id="quote-box">
+        <div id="text"><i className="fa fa-quote-right"></i><span>{this.state.quote}</span><i className="fa fa-quote-left"></i></div>
+        <div id="author">{this.state.author}</div>
+        <div id="btn-container">
+          <button id="new-quote" onClick={this.getQuote}>New quote</button>
+          <a id="tweet-quote" title="Tweet quote" target="_top" href={this.state.tweetLink}><i className="fa fa-twitter"></i></a>
+        </div>
+      </div>
+    );
+  }
+}
